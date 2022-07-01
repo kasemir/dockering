331f5b4fd22f54f569b880a8ed496f3a34ff1324
Kay Kasemir <kay.kasemir@gmail.com>
2022-07-01 15:13:32 -0400

    Two IOCs

--- epics_base/run_iocs.sh
+++ epics_base/run_iocs.sh
@@ -4,16 +4,18 @@
 # Could avoid this by running with --rm option,
 # but that would also remove logs of IOC on exit,
 # which are useful to debug problems
-docker rm ioc_demo
+docker rm ioc_ramp ioc_noise
 
 # -it     Create TTY for interactive use and/or for softIocPVA to continue
 # -d      Detach (remove to run in terminal)
 # --name  .. to identify IOC
 # -v      Mount './db' folder into container as /db
-docker run -itd --name ioc_demo --net=host -v $PWD/db:/db ornl_epics/epics_base softIocPVA -d /db/demo.db
+docker run -itd --name ioc_ramp --net=host -v $PWD/db:/db ornl_epics/epics_base softIocPVA -d /db/ramp.db
+docker run -itd --name ioc_noise --net=host -v $PWD/db:/db ornl_epics/epics_base softIocPVA -d /db/noise.db
 
 # Attach console via
 #
-#   docker attach ioc_demo
+#   docker attach ioc_ramp
+#   docker attach ioc_noise
 #
 # Exit attached console via CTRL-p CTRL-q
