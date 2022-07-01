425d158556d1f850a664cca27ef38a68bbf2423f
Kay Kasemir <kay.kasemir@gmail.com>
2022-07-01 14:08:10 -0400

    Running IOCs

--- epics_base/run_ioc.sh
+++ epics_base/run_ioc.sh
@@ -1,9 +1,19 @@
 # Run 'demo.db'
 
-# Interactively
-docker run -it --rm --net=host  -v $PWD:/db ornl_epics/epics_base softIocPVA -d /db/demo.db
+# In case IOC had been running, remove it.
+# Could avoid this by running with --rm option,
+# but that would also remove logs of IOC on exit,
+# which are useful to debug problems
+docker rm ioc_demo
 
-# In background
-# docker run --rm -d --name ioc_demo --net=host -v $PWD:/db ornl_epics/epics_base softIocPVA -d /db/demo.db
+# -it     Create TTY for interactive use and/or for softIocPVA to continue
+# -d      Detach (remove to run in terminal)
+# --name  .. to identify IOC
+# -v      Mount './db' folder into container as /db
+docker run -itd --name ioc_demo --net=host -v $PWD/db:/db ornl_epics/epics_base softIocPVA -d /db/demo.db
 
-
+# Attach console via
+#
+#   docker attach ioc_demo
+#
+# Exit attached console via CTRL-p CTRL-q
